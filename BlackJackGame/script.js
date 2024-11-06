c52ba26ce335a4df7f9a57bc3b1d9c6e1f5abb48
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2024-11-06 12:41:57 +0530

    made changes to the black Jack

--- BlackJackGame/script.js
+++ BlackJackGame/script.js
@@ -1,15 +1,37 @@
 // create two variables firstCard and secondCard and set their values to random num between 2-11
 
-let firstCard = 5
-let secondCard =20
-let sum =firstCard+secondCard
+
+let cards =[]
+let sum =0
 let hasBlackJack =false
-let isAlive =true
+let isAlive =false
 let messege =""
 let messegeEl=document.querySelector("#messege");
 let sumEl =document.getElementById("sum")
+cardsEl =document.getElementById("cards")
+
+function getRandomCard(){
+    myCard=Math.floor(Math.random()*13)+1
+    if(myCard===1){
+        return 11
+    }
+    else if(myCard===11 || myCard===12 ||myCard===13){
+        return 10
+    }
+    else{
+    return myCard
+    }
+}
 
 function startGame(){
+    let firstCard = getRandomCard()
+    let secondCard =getRandomCard()
+    cards =[firstCard,secondCard]
+    sum = firstCard+secondCard
+    renderGame()
+}
+
+function renderGame(){
     if(sum<=20){
         messege="Do you want to draw a new card?😊"
     }
@@ -21,10 +43,16 @@ function startGame(){
         messege="You are out of the game😭"
         isAlive =false
     }
-    console.log(messege)
-    console.log("has blackjack:"+hasBlackJack)
-    console.log("Is alive:"+isAlive)
     messegeEl.textContent=messege 
-sumEl.textContent="Sum:"+sum
-
+    for(let i=0;i<cards.length;i++){
+    cardsEl.textContent += cards[i]+" "
+    }
+    sumEl.textContent="Sum:"+sum
 }  
+
+function newCard(){
+let newCard =getRandomCard()
+sum+=newCard
+cards.push(newCard)
+renderGame()
+}
